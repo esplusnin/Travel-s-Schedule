4166ef2c097906ca24a35895a0fb55bd0f7cfd2a
esplusnin <esplusnin94@yandex.ru>
2024-02-29 19:13:09 +0400

    Initial Commit

---
+++ Travel's Schedule/Travel_s_ScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_s_ScheduleApp.swift
+//  Travel's Schedule
+//
+//  Created by Евгений on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Travel_s_ScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
