82b95017f50e8c28924c758657c164e6862b2a0a
esplusnin <esplusnin94@yandex.ru>
2024-03-15 10:35:03 +0400

    Удалить из гитигнор файл с окружением для ключей

--- Travel's Schedule/Resources/Environment.swift
+++ Travel's Schedule/Resources/Environment.swift
@@ -1,5 +1,6 @@
 import Foundation
 
 enum Environment {
-    static let yandexTrabelAPIKey = "6e3da6e0-2462-4952-84df-3380e32eb8f4"
+    // Указать свой ключ для тестирования
+    static let yandexTrabelAPIKey = ""
 }
